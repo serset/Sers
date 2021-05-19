751cf5c10dccf82a69fe52b020939dc00e6b3edc
lith <LithWang@yeah.net>
2021-05-19 02:14:54 +0000

    修改dockerfile文件，把app启动方式从startup.sh改为直接CMD启动

--- dotnet/Doc/PublishFile/SersDocker(2021-05-19)/pack SersDocker.bat
+++ dotnet/Doc/PublishFile/SersDocker(2021-05-19)/pack SersDocker.bat
@@ -6,30 +6,30 @@ xcopy  "..\PublishFile\SersDocker" "SersDocker" /e /i /r /y
 
 @echo "copy sers"
 xcopy  "SersPublish\%netVersion%\ServiceCenter\appsettings.json" "SersDocker\docker部署Sers\sers" 
-xcopy  "SersPublish\%netVersion%\ServiceCenter" "SersDocker\docker制作镜像Sers\sers\app" /e /i /r /y
+xcopy  "SersPublish\%netVersion%\ServiceCenter" "SersDocker\docker制作镜像Sers\sers\root\app\ServiceCenter" /e /i /r /y
 
 
 @echo "copy sers-demo"
 xcopy  "SersPublish\%netVersion%\Demo\appsettings.json" "SersDocker\docker部署Sers\sers-demo" 
-xcopy  "SersPublish\%netVersion%\Demo" "SersDocker\docker制作镜像Sers\sers-demo\app" /e /i /r /y
+xcopy  "SersPublish\%netVersion%\Demo" "SersDocker\docker制作镜像Sers\sers-demo\root\app\Demo" /e /i /r /y
 
 @echo "copy sers-demo-robot"
 xcopy  "SersPublish\%netVersion%\Robot\appsettings.json" "SersDocker\docker部署Sers\sers-demo-robot" 
-xcopy  "SersPublish\%netVersion%\Robot" "SersDocker\docker制作镜像Sers\sers-demo-robot\app" /e /i /r /y
+xcopy  "SersPublish\%netVersion%\Robot" "SersDocker\docker制作镜像Sers\sers-demo-robot\root\app\Robot" /e /i /r /y
 
 @echo "copy sers-demo-sersall"
 xcopy  "Sers压测\sers压测-单体压测%netVersion%\ServiceCenter\appsettings.json" "SersDocker\docker部署Sers\sers-demo-sersall" 
-xcopy  "Sers压测\sers压测-单体压测%netVersion%\ServiceCenter" "SersDocker\docker制作镜像Sers\sers-demo-sersall\app" /e /i /r /y
+xcopy  "Sers压测\sers压测-单体压测%netVersion%\ServiceCenter" "SersDocker\docker制作镜像Sers\sers-demo-sersall\root\app\ServiceCenter" /e /i /r /y
 
 
 
 @echo "copy sers-gateway"
 xcopy  "SersPublish\%netVersion%\Gateway\appsettings.json" "SersDocker\docker部署Sers\sers-gateway" 
-xcopy  "SersPublish\%netVersion%\Gateway" "SersDocker\docker制作镜像Sers\sers-gateway\app" /e /i /r /y
+xcopy  "SersPublish\%netVersion%\Gateway" "SersDocker\docker制作镜像Sers\sers-gateway\root\app\Gateway" /e /i /r /y
 
 @echo "copy sers-gover"
 xcopy  "SersPublish\%netVersion%\Gover\appsettings.json" "SersDocker\docker部署Sers\sers-gover" 
-xcopy  "SersPublish\%netVersion%\Gover" "SersDocker\docker制作镜像Sers\sers-gover\app" /e /i /r /y
+xcopy  "SersPublish\%netVersion%\Gover" "SersDocker\docker制作镜像Sers\sers-gover\root\app\Gover" /e /i /r /y
 
 
 
