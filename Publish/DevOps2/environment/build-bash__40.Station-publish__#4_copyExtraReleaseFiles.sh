c8cc30d7b33cb64c1b0d4c9619136cf48875703b
lith <LithWang@yeah.net>
2024-01-15 00:01:04 +0800

    fix

--- Publish/DevOps2/environment/build-bash__40.Station-publish__#4_copyExtraReleaseFiles.sh
+++ Publish/DevOps2/environment/build-bash__40.Station-publish__#4_copyExtraReleaseFiles.sh
@@ -16,7 +16,7 @@ export publishPath=\"$basePath/Publish/release/release/Station($netVersion)\"
 echo '#4 copy extra release files'
 
 mkdir -p "$basePath/Publish/release/release/ServiceCenter($netVersion)"
-cp -rf "$publishPath/ServiceCenter/. "$basePath/Publish/release/release/ServiceCenter($netVersion)/ServiceCenter"
+cp -rf "$publishPath/ServiceCenter/." "$basePath/Publish/release/release/ServiceCenter($netVersion)/ServiceCenter"
 cp -rf "$publishPath/01.ServiceCenter.bat" "$basePath/Publish/release/release/ServiceCenter($netVersion)"
 cp -rf "$publishPath/01.Start-4580.bat" "$basePath/Publish/release/release/ServiceCenter($netVersion)"
 
